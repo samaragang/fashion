0dfdbda5f93aa372fda5de9364220d3ef6e7c7aa
samaragang <dmitr.burii.2016@gmail.com>
2023-07-15 13:41:52 +0300

    promo completed

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -1,8 +1,35 @@
+import './promo.css'
+import promoImg from './../../img/images/header-img.jpg'
 const Promo = () => {
-    return (
-        <>
-        </>
-    )
+  return (
+    <section className="promo">
+      <div className="container">
+        <div className="promo__content">
+          <div className="promo__text">
+            <div className="promo__title">
+              <span className='highlight'>
+                <span>LET’S</span>
+              </span>
+              EXPLORE
+              <span className='highlight highlight--yellow'>
+                <span>UNIQUE</span>
+              </span>
+              CLOTHES.
+            </div>
+            <div className="promo__description">
+              Live for Influential and Innovative fashion!
+            </div>
+            <div className="promo__btn-wrapper">
+              <a href="/">Shop Now</a>
+            </div>
+          </div>
+          <div className="promo__image">
+            <img src={promoImg} alt="promoImg" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Promo;
